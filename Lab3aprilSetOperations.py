af79abc4c22ed54ad83801c396aefd5b9b1e4e97
Arvindgit-tech <akumarpal92@gmail.com>
2025-04-08 11:18:27 +0530

    first

--- Lab3aprilSetOperations.py
+++ Lab3aprilSetOperations.py
@@ -1,6 +1,17 @@
 
-a= {1,2,3}
+a= {1,2,3, 10}
 b= {3,4,5}
 print(a|b)   #Union 
 
 print(a&b)  #Intersection
+
+print(a-b) 
+print(b-a)
+
+print(a^b)
+
+FS = frozenset ([10,20,30])
+print(FS)
+
+print(FS-a)
+
