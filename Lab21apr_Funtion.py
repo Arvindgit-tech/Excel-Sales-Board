42cff8b57380b129d8394f3c7e754a660a5ce47a
Arvindgit-tech <akumarpal92@gmail.com>
2025-04-21 17:57:12 +0530

    filehandling_commit

---
+++ Lab21apr_Funtion.py
@@ -0,0 +1,24 @@
+
+def Greet(Name):
+    print(f"Hello {Name}")
+
+
+Greet("Arvind")
+
+def hello ():
+    print("This is funtions welcome you")
+
+hello()
+
+
+def square (Num):
+    return Num **2
+
+print(square (8))
+
+
+def greet(name = "Ravi"):
+ return f"good Morning {name}"
+print(greet())
+
+
