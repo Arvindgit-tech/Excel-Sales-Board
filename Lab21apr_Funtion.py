9c84730e375a308a666e593e980bda351e890909
Arvindgit-tech <akumarpal92@gmail.com>
2025-05-06 11:27:02 +0530

    loops

--- Lab21apr_Funtion.py
+++ Lab21apr_Funtion.py
@@ -17,8 +17,19 @@ def square (Num):
 print(square (8))
 
 
-def greet(name = "Ravi"):
+def greet(name = "Dear"):
  return f"good Morning {name}"
-print(greet())
+print(greet("Arvind"))
 
 
+def add(*hii): # *args
+ return sum (hii)
+print(add(232,43435,4,54,5,4,6,56))
+
+
+def factorial(n):
+ if n == 1:
+    return 1
+ else:
+    return n * factorial(n-1)
+print(factorial(6))
