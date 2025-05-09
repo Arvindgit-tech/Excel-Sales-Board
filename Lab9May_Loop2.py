b0b3ad4c9fc345e0c7ade6e8153d0f223bcc8b71
Arvindgit-tech <akumarpal92@gmail.com>
2025-05-09 10:07:14 +0530

    loops2

--- Lab9May_Loop2.py
+++ Lab9May_Loop2.py
@@ -1,1 +1,22 @@
 
+
+# Problem: Print the multiplication table for a given number upto 10,but
+#  skip the fifth iteration.
+
+# n= int(input("enter the number "))
+# for i in range (1, 11):
+#     temp= n*i
+#     if i!=5:
+#         print(f' {n}*{i}={temp}')
+
+
+# Problem: Print the multiplication table for a given number upto 10,but
+# #  skip the fifth multiple iteration.
+
+n= int(input("enter the number "))
+for i in range (1, 101):
+    temp= n*i
+    if i%5!=0:
+        print(f' {n}*{i}={temp}')
+
+        
