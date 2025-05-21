d1043bdfc31d6d90f7b13f5e7f92e12e282eac30
Arvindgit-tech <akumarpal92@gmail.com>
2025-05-21 11:02:26 +0530

    21mayNumPy

---
+++ Lab15May_NumPy.py
@@ -0,0 +1,16 @@
+
+#import panda as pd
+import numpy as np
+#import seaborn as sb
+#import matplotlib as mp
+
+A= np.array([1,2,3,4,5,7,4,5,6,8,9,10])
+print(A)
+print(A.mean())
+print(A.ndim)
+
+tens = np.array([[[[[2,3,33,4,45],[23,45,96,66,2],[50,89,528,968,789],[32,85,74,12,3]]]]])
+print(tens.ndim)
+
+x=np.array([10,20,30],dtype = str)
+print(x)
---
+++ Lab19may_Numpy.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+a= np.array([10,20,30], dtype=float)
+a1= np.array([10,20,30], dtype=str)
+a2= np.array([10,20,30], dtype=int)
+a3= np.array([10,20,30], dtype=complex)
+a4= np.array([10,20,30, 0, -3], dtype=bool)
+a5= np.arange(5,30)
+a6= np.arange(2,41, 2)
+a7= np.arange(2,44).reshape(7,6)
+a8= np.ones((5,4))
+a9= np.zeros((5,4))
+a10= np.random.random((5,4))
+a11= np.identity(3)
+
+
